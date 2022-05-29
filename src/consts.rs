fa827c47ebf202a62dd85ac44c836369b9a21937
J-wyt <546108612@qq.com>
2022-05-30 01:04:57 +0800

    Add user agent

--- src/consts.rs
+++ src/consts.rs
@@ -1,1 +1,2 @@
 pub const URL: &'static str = "https://www.bilibili.com/video/";
+pub const UA: &'static str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36";
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use consts::URL;
-
 mod consts;
 mod downloader;
 
 fn main() -> Result<(), ureq::Error> {
     let b_id = "bvidexample";
-    let url = URL.to_string() + b_id;
-    let r = ureq::get(url.as_str()).call()?.into_string()?;
+    let url = consts::URL.to_string() + b_id;
+    let r = ureq::get(url.as_str())
+        .set("user-agent", consts::UA)
+        .call()?
+        .into_string()?;
     println!("{}", r);
     Ok(())
 }
